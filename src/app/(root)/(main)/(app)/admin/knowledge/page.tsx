179defad961046a986e8ac26324db722afd0ac12
kevsjh <kevsjh@gmail.com>
2024-04-25 10:27:08 +0800

    Refactor admin knowledge page to include table component and update import statements

--- src/app/(root)/(main)/(app)/admin/knowledge/page.tsx
+++ src/app/(root)/(main)/(app)/admin/knowledge/page.tsx
@@ -1,19 +1,61 @@
 import { getUseAdminKnowledge } from "@/lib/auth/admin-action";
 import { AddContentDialog } from "./components/add-content-dialog";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Badge } from "@/components/ui/badge";
+
 
 export default async function Page() {
 
     const { contents } = await getUseAdminKnowledge()
     console.log('content', contents)
 
-    return <div className="flex  flex-col h-full p-6   w-full">
+    return <div className="flex  flex-col h-full p-6  gap-6  w-full">
         <div className="flex items-center justify-between"><h1 className="text-3xl font-semibold">Knowledge Hub</h1>
             <AddContentDialog />
 
         </div>
+        <Table>
 
-        <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 gap-6">
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="">Title</TableHead>
+                    <TableHead>File Type</TableHead>
+                    {/* <TableHead>Method</TableHead> */}
+                    <TableHead className="text-right">Date</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {
+                    contents.map(content => {
+                        return (
+                            <TableRow key={content.id}>
+                                <TableCell>{content.userFilename}</TableCell>
+                                <TableCell>
+                                    {
+                                        content.type &&
+                                        <Badge>
+                                            {
+                                                content?.type?.charAt(0)?.toUpperCase() + content?.type?.slice(1)
+                                            }
+                                        </Badge>
+                                    }
+                                </TableCell>
+                                {/* <TableCell>{content.uid}</TableCell> */}
+                                <TableCell className="text-right">{content.createdAt.toDateString()}</TableCell>
+                            </TableRow>
+                        );
+                    })
+                }
+            </TableBody>
+        </Table>
 
-        </div>
     </div>
 }
