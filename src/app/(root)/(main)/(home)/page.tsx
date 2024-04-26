a00f084c117f7b12a5f1f33ade00abc411a18055
kevsjh <kevsjh@gmail.com>
2024-04-26 12:34:51 +0800

    Add embla-carousel-react dependency and update home page styles

--- src/app/(root)/(main)/(home)/page.tsx
+++ src/app/(root)/(main)/(home)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="pt-36 space-y-4 px-4 sm:px-12 text-white drop-shadow-2xl max-w-screen-xl w-full">
 
           <Badge className="text-sm">Submission by team kecyi</Badge>
-          <h1 className="  tracking-wide  text-4xl md:text-5xl  lg:text-6xl font-bold text-white drop-shadow-2xl">
+          <h1 className=" bg-black/10 w-fit p-4 rounded-2xl tracking-wide  text-4xl md:text-5xl  lg:text-6xl font-bold text-white drop-shadow-2xl">
             Next Gen AI Customer Service<br /> and Agent Based Assistant for <br />Banking & Financial Services
           </h1>
           <p className="drop-shadow-2xl font-medium">This prototype project is built for Google Cloud GenAI Hackathon | APAC</p>
@@ -32,6 +32,17 @@ export default function Home() {
 
           <div className="w-fit">
             <AccessPortalButton />
+          </div>
+
+
+          <div className="pt-24 flex flex-col items-center w-full">
+            <div className="p-6 rounded-2xl shadow-md bg-black/20">
+              <p className="text-3xl font-semibold text-center drop-shadow-xl">
+                Elevate beyond traditional chatbot and conversations
+              </p>
+            </div>
+
+
           </div>
         </div>
 
