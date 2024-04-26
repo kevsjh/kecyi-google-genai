0674948a1000ad47d2817f5baec30d37f0dc70bc
kevsjh <kevsjh@gmail.com>
2024-04-26 23:02:59 +0800

    Update robots.ts to use new sitemap URL and remove disallow rule for '/profile/'

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,9 +6,9 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: '/profile/',
+
       },
     ],
-    sitemap: `https://newroom.io/sitemap.xml`,
+    sitemap: `https://genai.kecyi.com/sitemap.xml`,
   };
 }
